94cfc85794ff1fe69c180829cab9f7f4c7c64c5c
Stas00001 <116278430+Stas00001@users.noreply.github.com>
2024-03-31 14:55:06 +0300

    fix: url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "http://api.kupipodariday.makeev.nomoredomainswork.ru";
+export const URL = "https://api.kupipodariday.makeev.nomoredomainswork.ru";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
